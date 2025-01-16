7d2f1579d6f1a3127140c67fc26d945e06bdcfd9
guwls <guswls2654@naver.com>
2025-01-16 21:59:52 +0900

    엔티티 개발 - 연관관계 없음

--- src/main/kotlin/com/hyunjin/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/hyunjin/portfolio/domain/entity/Achievement.kt
@@ -1,13 +1,31 @@
 package com.hyunjin.portfolio.domain.entity
 
 import jakarta.persistence.*
+import org.springframework.context.annotation.Description
+import java.time.LocalDate
 
 @Entity
-class Achievement : BaseEntity() {
+class Achievement(
+    title: String,
+    description: String,
+    achievement: LocalDate?,
+    host: String,
+    isActive: Boolean
+    ) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "achievement_id")
     var id: Long? = null
+
+    var title: String = title
+
+    var description: String = description
+
+    var achievement: LocalDate? = achievement
+
+    var host: String = host
+
+    var isActive: Boolean = isActive
 
 }

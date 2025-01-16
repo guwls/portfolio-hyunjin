05df2dd63565b4e7ba01025f91cb1c4e8dd67aa3
guwls <guswls2654@naver.com>
2025-01-16 16:10:06 +0900

    Git과 Github

---
+++ src/test/kotlin/com/hyunjin/portfolio/PortfolioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.hyunjin.portfolio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

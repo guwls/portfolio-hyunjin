33ba08de6afdfceb9064e01b6a4777f3a9078284
guwls <guswls2654@naver.com>
2025-01-27 14:41:09 +0900

    DTO 개발

--- src/main/kotlin/com/hyunjin/portfolio/presentation/dto/ProjectDTO.kt
+++ src/main/kotlin/com/hyunjin/portfolio/presentation/dto/ProjectDTO.kt
@@ -1,3 +1,22 @@
 package com.hyunjin.portfolio.presentation.dto
 
-class ProjectDTO
+import com.hyunjin.portfolio.domain.entity.Project
+
+data class ProjectDTO(
+    val name: String,
+    val description: String,
+    val startYearMonth: String?,
+    val endYearMonth: String?,
+    val details: List<ProjectDetailDTO>,
+    val skills: List<SkillDTO>
+){
+    constructor(project: Project): this(
+        name = project.name,
+        description = project.description,
+        startYearMonth = "${project.startYear}.${project.startMonth}",
+        endYearMonth = project.getEndYearMonth(),
+        details = project.details.filter{ it.isActive }.map { ProjectDetailDTO(it) }, // true인 것만 넘어옴
+        skills = project.skills.map { it.skill }.filter { it.isActive }.map { SkillDTO(it)}
+
+    )
+}
--- src/main/kotlin/com/hyunjin/portfolio/presentation/dto/SkillDTO.kt
+++ src/main/kotlin/com/hyunjin/portfolio/presentation/dto/SkillDTO.kt
@@ -1,3 +1,15 @@
 package com.hyunjin.portfolio.presentation.dto
 
-class SkillDTO
+import com.hyunjin.portfolio.domain.entity.Skill
+import javax.print.DocFlavor.STRING
+
+data class SkillDTO(
+    val name: String,
+    val type: String
+){
+    constructor(skill: Skill): this(
+        name = skill.name,
+        type = skill.type.name
+    )
+
+}
